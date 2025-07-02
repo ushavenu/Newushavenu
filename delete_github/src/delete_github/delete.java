024cc9be39cf196f858add9bfb618ed6c120f275
VENUGOBAL <VENUGOBAL@LAPTOP-H4MMLA92>
2025-07-02 14:19:32 +0530

    first commit

---
+++ delete_github/src/delete_github/delete.java
@@ -0,0 +1,10 @@
+package delete_github;
+
+public class delete {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("sending file to vandana");
+	}
+
+}
